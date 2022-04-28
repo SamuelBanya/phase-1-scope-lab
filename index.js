83e175630715f7d282f3ba70aba0d0fceb8c7186
Samuel Banya <sbanya@fastmail.com>
2022-04-28 19:11:14 -0400

    commit tree 5671f57dac6fbcc9b9b674a146b6c322396dcf8c

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+
+    return customerName.toUpperCase(0);
+}
+
+var bestCustomer;
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+    
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+
+    return bestCustomer;
+}
+
+const leastFavoriteCustomer = "Tim";
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "Sam";
+}
+
+
